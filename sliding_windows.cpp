a5442dc1ebdd97ac95ba5b687dd2298d8445223f
Pan, Zhiyi <zhiyi.pan@intel.com>
2021-05-07 14:03:37 +0800

    hashmap sliding window

--- sliding_windows.cpp
+++ sliding_windows.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 // 1.Longest Substring Without Repeating Characters
-//sliding window
+//sliding window O(n2)
 int lengthOfLongestSubstring(string s) {
     int left = 0, maxlen = 0, len = 0;
     set<char> chset;
@@ -17,6 +17,23 @@ int lengthOfLongestSubstring(string s) {
                 left = j+1;
             }
         }
+        if(len>maxlen)maxlen=len;
+    }
+    return maxlen;
+}
+//hashmap sliding windows O(n)
+int lengthOfLongestSubstring(string s) {
+    int left = 0, maxlen = 0, len = 0;
+    map<char, int> mp;
+    if(s=="")return 0;
+    for(int i=0; i<s.length();i++ ){
+        
+        if(mp.find(s[i])!=mp.end()){
+            left = max(mp[s[i]],left);
+            mp[s[i]] = i+1;
+        
+        }else mp[s[i]] = i+1;
+        len = i-left+1;
         if(len>maxlen)maxlen=len;
     }
     return maxlen;
